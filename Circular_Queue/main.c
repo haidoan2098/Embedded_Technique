c3fcb027bdd527a357200a37ef580fc4baafbed5
Đoàn Phú Hải <157389439+haidoan2098@users.noreply.github.com>
2024-03-31 10:02:14 +0700

    Update main.c

--- Circular_Queue/main.c
+++ Circular_Queue/main.c
@@ -1,3 +1,11 @@
+/* Circular Queue (Hàng đợi vòng): Là CTDT thực hiện theo cơ chế FIFO. Là phiên bản cải tiến của Hàng đợi tiêu chuẩn.
+Sự khác nhau:
+ - Queue: Sau khi ptử bị đẩy ra thì vị trí bị đẩy sẽ ko được tái sử dụng khi tất cả bị trong Queue bị đẩy hết ra.
+ - Circular Queue: Vị trí phần tử bị đẩy ra sẽ được tái sử dụng.
+Queue được sử dụng khi nào? 
+  Khi ta cần 1 vùng nhớ nhỏ mà vẫn xử lí dữ liệu với đầu vào liên tục.
+*/
+
 #include "Circular_Queue.h"
 #include <stdio.h>
 
