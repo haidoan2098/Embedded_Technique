e58aed306422e6d8d79e441f280e4acc7f4ce86a
Đoàn Phú Hải <157389439+haidoan2098@users.noreply.github.com>
2024-04-04 10:19:28 +0700

    Add files via upload

--- State Machine/State_Machine.c
+++ State Machine/State_Machine.c
@@ -1,20 +1,11 @@
-/* State Machine (Trạng thái máy): Là một mô hình tính toán dựa trên một máy giả định được tạo thành từ một hoặc
-nhiều trạng thái. Nó chuyển từ trạng thái này sang trạng thái khác theo tuần tự nhất định.
-Khi nào nên dùng State Machine?
-    Hệ thống có nhiều trạng thái và phức tạp, ta muốn chia thành các phần nhỏ hơn độc lập nhau.
-Lợi ích: 
-    + Dễ dàng theo dõi quá trình chuyển đổi.
-    + Bảo trì dễ dàng, các trạng thái độc lập nhau. 
-Ví dụ: Bằng một nút nhấn, khi nhấn nút 1 lần thì mở Mode_1, nhấn lần 2 thì mở Mode_2, nhấn lần 3 thì mở Mode_3, nhấn lần 4 thì 3 Mode Off */
-
 #include <stdio.h>
 #include <stdint.h>
 
 typedef enum{
-    MODE_1, //<=> 0
-    MODE_2, //<=> 1
-    MODE_3, //<=> 2
-    MODE_OFF
+    MODE_1,   //<=> 0
+    MODE_2,   //<=> 1
+    MODE_3,   //<=> 2
+    MODE_OFF  //<=> 3
 } MODE_T;
 
 MODE_T mode_t; //mode_t mặc định là 0
@@ -22,16 +13,16 @@ MODE_T mode_t; //mode_t mặc định là 0
 
 //-----------FUNC MODE---------------------
 void func_mode_1(){
-    printf("Turn on mode 1\n");
+    printf("LED 1 sáng\n");
 }
 void func_mode_2(){
-    printf("Turn on mode 2\n");
+    printf("LED 2 sáng\n");
 }
 void func_mode_3(){
-    printf("Turn on mode 3\n");
+    printf("Cả 2 LED chớp tắt\n");
 }
 void func_mode_off(){
-    printf("3 MODE OFF\n");
+    printf("Tắt cả 2 LED\n");
 }
 
 
@@ -43,22 +34,22 @@ void button_press_hold(){
     scanf("%d", &button);
 
     if(button == 1){
-    switch (mode_t){
-        case MODE_1:
-            func_mode_1();
-            break;
-        case MODE_2:
-            func_mode_2();
-            break;
-        case MODE_3:
-            func_mode_3();
-            break;    
-        case MODE_OFF:
-            func_mode_off();
-            break; 
-        default:
-            break;
-    }
+        switch (mode_t){
+            case MODE_1:
+                func_mode_1();
+                break;
+            case MODE_2:
+                func_mode_2();
+                break;
+            case MODE_3:
+                func_mode_3();
+                break;    
+            case MODE_OFF:
+                func_mode_off();
+                break; 
+            default:
+                break;
+        }
     }
 }
 
